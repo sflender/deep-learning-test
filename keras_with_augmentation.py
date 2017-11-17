3516e18bc4e2916dc798b793ef2292498ec60aee
Samuel Flender <flender@cooleylogin2.fst.alcf.anl.gov>
2017-11-17 21:53:40 +0000

    modify keras-test.py, add augmentation python code

--- keras_with_augmentation.py
+++ keras_with_augmentation.py
@@ -9,10 +9,10 @@ from keras.utils import plot_model
 from keras import optimizers
 import matplotlib.image as mpimg
 import pandas as pd
-import os
+from keras.preprocessing.image import ImageDataGenerator
+
 
 def build_model1():
-	# this is a convolutional model with 1 hidden "layer" (consisting of convolution + pooling)
 	model = Sequential()
 	model.add(Conv2D(32,(3,3), activation='relu', padding='same', input_shape=(28,28,1,)))
 	model.add(MaxPooling2D(pool_size=(3,3)))
@@ -21,7 +21,6 @@ def build_model1():
 	return model
 
 def build_model2():
-	# a more complex model with 3 hidden layers (conv+pool --> conv+pool --> dense --> dense)
 	model = Sequential()
 	model.add(Conv2D(32,(3,3), activation='relu', padding='same', input_shape=(28,28,1,)))
 	model.add(MaxPooling2D(pool_size=(2,2)))
@@ -32,25 +31,20 @@ def build_model2():
 	model.add(Dense(10, activation='softmax'))
 	return model
 
-# first make sure that "output" folder exists where we will write the output.
-if not os.path.exists("output"):
-	print "creating output folder"
-	os.makedirs("output")
 
-# load the mnist dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 print "X_train shape", X_train.shape
 print "y_train shape", y_train.shape
 print "X_test shape", X_test.shape
 print "y_test shape", y_test.shape
 
-# reshape the data into the form that keras expects
+# reshape
 X_train = X_train.reshape( X_train.shape[0], X_train.shape[1], X_train.shape[2], 1 )
 X_test = X_test.reshape( X_test.shape[0], X_test.shape[1], X_test.shape[2], 1 )
 #X_train = X_train.reshape(60000, 784)
 #X_test = X_test.reshape(10000, 784)
 
-# normalize and convert to floats
+# normalize
 X_train = X_train.astype('float32') / 255.0
 X_test = X_test.astype('float32') / 255.0
 
@@ -63,20 +57,39 @@ Y_test = np_utils.to_categorical(y_test, 10)
 
 # build the model
 model = build_model2()
-model.summary()
 
 # compile the model
+model.summary()
 optimizer = optimizers.SGD(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['acc'])
 
-# train the model
-history = model.fit([X_train,], Y_train, batch_size=32, nb_epoch=100, validation_split=0.33, verbose=1)
+# --- image augmentation
+aug_rot = 15 #augmentation rotation in degrees
+aug_shift = 0.1 #augmentation shift
+aug_shear = 0.1 # image shear augmentation
+aug_zoom = 0.1 # image zoom augmentation
 
-# evaluate the model on the test set
+datagen = ImageDataGenerator(rotation_range=aug_rot, 
+                             width_shift_range=aug_shift, 
+                             height_shift_range=aug_shift,
+                             zoom_range = aug_zoom,
+                             shear_range = aug_shear)
+
+#--- instead of model.fit, use model.fit_generator, with datagen.flow(...) to generate the data per batch!
+#history = model.fit([X_train,], Y_train, batch_size=32, nb_epoch=50, validation_split=0.33, verbose=1)
+batch_size = 32
+epochs = 100
+
+history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size), 
+							  steps_per_epoch=40000/batch_size,
+						      validation_data=datagen.flow(X_train, Y_train, batch_size=batch_size),
+                    		  validation_steps=20000/batch_size, 
+                    		  epochs=epochs)
+
 score = model.evaluate(X_test, Y_test, verbose=0)
 print 'Test score:', score
 
-model_name = "CNN_model2"
+model_name = "CNN_model2aug"
 
 with open("output/"+model_name+"_test_score.txt", 'w') as f:
     print >> f, score
